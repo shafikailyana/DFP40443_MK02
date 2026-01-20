46fbcbfa824e528c05f5152637b0cfec34050b4b
NUR SHAFIKA ILYANA <18dit24f1002@student.ptss.edu.my>
2026-01-20 09:32:18 +0800

    first commit nothing changes

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+$nama = "NUR SHAFIKA ILYANA";
+$nomatrik = "18DIT24F1002";
+$kelas = "DIT4A/SWG1";
+?>
+
+<!DOCTYPE html> 
+<html>
+<head>
+<title›Biodata Pelajar</title>
+</head>
+<body>
+
+<h1>Biodata Saya</h1>
+
+<p><b>Nama: </b><?= $nama ?></p> 
+<p><b>No Matrik: </b><?= $nomatrik ?></p>
+<p><b>Kelas: </b><?= $kelas ?></p>
+
+<img src="shafika.jpg" style="width:200px; height:200px; border-radius:50%; object-fit:cover;">
+
+</body>
+</html>
